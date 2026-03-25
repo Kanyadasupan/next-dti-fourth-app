5de31d1a27cf1e3992df89c6114404aa81cecb50
KanyadaSupan <s6652410028@sau.ac.th>
2026-03-25 17:00:37 +0700

    update next.js version

--- app/bmi/page.tsx
+++ app/bmi/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import bmi from "../images/bmi.png";
 import Link from "next/link";
 import { useState } from "react";
-import { parse } from "path";
+
 
 export default function Page() {
 
